d05da1c4e847c3d7fd2a864c68a65dc290effa40
Davide <davidebadolato@tiscali.it>
2019-08-19 12:12:37 +0200

    Aggiunti configdata per fabbriche e ricerca lavoro

---
+++ Assets/Scripts/ConfigData/Factory/FactoryConfigData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Bado_City
+{
+    /// <summary>
+    /// Classe per creare accoglienti posti di lavoro
+    /// </summary>
+    [CreateAssetMenu(fileName = "NewFactoryConfigData", menuName = "Factory/NewFactory", order = 0)]
+    public class FactoryConfigData : ScriptableObject
+    {
+        public List<QuizConfigData> PossibQuestions = new List<QuizConfigData>();
+        public int MaxQuestions;
+        [HideInInspector]
+        public List<QuizConfigData> CurrentQuiz = new List<QuizConfigData>();
+        public FactoryInfo info;
+        [System.Serializable]
+        public struct FactoryInfo
+        {
+            public string Name; //Nome della fabbrica           
+            public List<string> Descriptions; // Lista nel caso si volesse dividere la descrizione in più parti
+            public Sprite IconSprite; // Immagine della fabbrica
+        }
+    }
+}
---
+++ Assets/Scripts/ConfigData/Factory/QuizConfigData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bado_City
+{/// <summary>
+/// Classe per creare domande a risposta multipla
+/// </summary>
+    [CreateAssetMenu(fileName = "NewQuizConfigData", menuName = "Factory/NewQuiz", order = 1)]
+    public class QuizConfigData : ScriptableObject
+    {
+        public string Question;
+        public List<string> Answers = new List<string>();
+        public List<int> CorrectAnswerListIndex = new List<int>(); // Prima avevo messo solo int, alla fine ho optato per una lista nel caso si volessero formulare domande con più risposte corrette
+    }
+}
