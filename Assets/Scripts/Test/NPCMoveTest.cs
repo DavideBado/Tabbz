ed3b665ae59972a1d885ce9cf2baed9ca062ff42
Davide <davidebadolato@tiscali.it>
2019-08-28 05:10:01 +0200

    Aggiunto modello per distributore giochi

---
+++ Assets/Scripts/Test/NPCMoveTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMoveTest : MonoBehaviour
+{
+    public float Speed;
+    Vector3 CurrentTarget;
+    int i = 0;
+    public List<Vector3> targets = new List<Vector3>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentTarget = targets[i]; // Ecco la mia prima destinazione
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, CurrentTarget, Speed * Time.deltaTime); // Mi muovo verso la mia destinazione
+        if (transform.position == CurrentTarget) // Quando arrivo a destinazione
+        {
+            if (i == (targets.Count - 1)) // Se era l'ultima destinazione
+            {
+                i = 0; // Torno alla prima
+            }
+            else i++; // Se non lo era allora passo alla prossima
+            CurrentTarget = targets[i]; // Aggiorno la destinazione
+        }
+    }
+
+}
